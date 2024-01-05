db0c3cca20e742384ff4a72d130396e517c7a9a1
psylva79 <122635591+psylva79@users.noreply.github.com>
2024-01-05 21:55:52 +0000

    Add files via upload

---
+++ CodeBook.md
@@ -0,0 +1,7 @@
+The final database has six variables:
+* activity: type of activity
+* subject:- number of the subject 
+* type: type of signal obtained
+* stat: statistic computed, i.e., mean or std
+* xyz: group
+* value for the statistic
--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,6 @@
-# Import the tidyverse
-
 library(tidyverse)
 
-# import data: X_train and X_test (contains the explanatory variables), Y_train and Y_test (contains activity), subject_train and subject_test (contains subject), activity_labels, and features.
-# read_table() from the readr package is used to import datasets 
+# import data
 X_train <- read_table("C:/Users/pmps/Downloads/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt", 
                       col_names = FALSE)
 
@@ -14,12 +11,13 @@ subject_train <- read_table("C:/Users/pmps/Downloads/getdata_projectfiles_UCI HA
                             col_names = "subject")
 
 
-features <- read_table("C:/Users/pmps/Downloads/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt",                        col_names = FALSE)
+features <- read_table("C:/Users/pmps/Downloads/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt", 
+                       col_names = FALSE)
 
 activity_labels <- read_table("C:/Users/pmps/Downloads/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt", 
                               col_names = FALSE)
 
-
+colnames(X_train) <- features$X2
 
 X_test <- read_table("C:/Users/pmps/Downloads/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt", 
                      col_names = FALSE)
@@ -30,8 +28,6 @@ Y_test <- read_table("C:/Users/pmps/Downloads/getdata_projectfiles_UCI HAR Datas
 subject_test <- read_table("C:/Users/pmps/Downloads/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt", 
                            col_names = "subject")
 
-# Change column names of X_train and X_test with features vector
-colnames(X_train) <- features$X2
 names(X_test) <- features$X2
 
 ##########################################################################################################
@@ -40,16 +36,15 @@ X <- X_train %>% bind_rows(X_test, .id = "id")
 Y <- Y_train %>% bind_rows(Y_test)
 subject <- subject_train %>% bind_rows(subject_test) 
 
-# select columns with "mean()" & "std()" in colnames. Here I use REGEX expression because "()" needs special syntax
-# anyof() inside select to filter columns
+# select columns with "mean" & "std" in colnames
 relevant_cols <- grep(pattern = "(mean|std)\\(\\)", features$X2, value = TRUE)
 X <- X %>% select(any_of(relevant_cols))
 
-# merge activity, subject and X. I use bind_cols() from dplyr, but rbind() or add_columns() would also work
+# merge activity, subject and X
 YX <- bind_cols(activity = Y$activity, subject = subject$subject) %>% 
   bind_cols(X)
 
-# change activity in the dataframe. Here, I use inner_join to obtain activity labels. One alternative would be to pass the labels inside the factor() function, with labels equal to activity_labels$X2: factor(activity_labels, levels = activity_labels$X1, labels = activity_labels$X2)
+# change activity in the dataframe
 YX <- YX  %>% 
   inner_join(activity_labels, by = c("activity" = "X1")) %>% 
   relocate(X2, .before = 1) %>%
@@ -57,11 +52,10 @@ YX <- YX  %>%
   rename(activity = X2)  %>% 
   mutate_at(activity:subject, as.factor)
 
-# transform to long format. In my opinion, it is preferable to work with long format with this data. One observation per row
+# transform to long format. In my opinion, it is preferable to work with long format with this data
 YX_long <-  YX %>% 
   pivot_longer(3:68, names_to = c("type", "stat", "xyz"), names_sep = "-", names_repair = "minimal")
 
-# Here I change the type variable (previously in column names)
 YX_long <- YX_long  %>% 
   mutate(stat = gsub("\\(\\)", "", stat),         
          type = gsub("Acc", " Accelerometer ", type),
@@ -77,7 +71,7 @@ YX_long <- YX_long  %>%
 
 write.table(YX_long, "YX_long.txt", row.name=FALSE)
 
-# Lastly I use summarize to obtain grouped statistics
+
 finaldf <- YX_long %>% 
 group_by(subject, activity, type, stat, xyz) %>%
   summarise(mean =mean(value, na.rm =TRUE))
